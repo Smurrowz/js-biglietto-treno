62ce743a0b1386025cffc584d7a9cfc754744fa3
Smurrowz <nigeldn1995@gmail.com>
2022-07-20 14:53:41 +0200

    esercizio completato

---
+++ app.js
@@ -0,0 +1,39 @@
+// chiedo l'eta' del passeggero
+
+const etaPasseggero = parseInt(prompt('quanti anni hai?') ); 
+console.log(etaPasseggero);
+
+// chiedo i km che il passeggero deve percorrere
+
+const kmPasseggero = parseInt(prompt('mi dici il numero di km che devi percorrere?') ) ;
+console.log(kmPasseggero);
+
+// setto la tariffa
+
+const prezzoAlKm = 0.21;
+
+// calcolo il prezzo finale
+
+let prezzoFinale;
+
+if (etaPasseggero < 18) {
+  console.log('il passeggero e minorenne');
+  const prezzoBase = prezzoAlKm * kmPasseggero;
+  prezzoFinale = prezzoBase - (prezzoBase * 0.2).toFixed(2);
+  console.log('il costo del biglietto e:€ ' + prezzoFinale);
+  
+} else if ( etaPasseggero > 65) {
+  console.log('il passeggero e over65');
+  const prezzoBase = prezzoAlKm * kmPasseggero;
+  prezzoFinale = prezzoBase - (prezzoBase * 0.4).toFixed(2);
+  console.log('il costo del biglietto e:€ ' + prezzoFinale);
+  
+} else {
+  console.log('il passeggero non ha diritto a nessuno sconto');
+  const prezzoBase = prezzoAlKm * kmPasseggero;
+  prezzoFinale = prezzoBase.toFixed(2);
+  console.log('il costo del biglietto e:€ ' + prezzoFinale);
+  
+}
+
+prezzo.innerHTML = prezzoFinale;
